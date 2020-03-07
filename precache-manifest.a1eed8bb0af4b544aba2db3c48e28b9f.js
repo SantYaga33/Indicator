de73234babce3cd7c68409ee77f58f9531fcbbaa
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-03-07 22:23:44 +0400

    Updates

---
+++ precache-manifest.a1eed8bb0af4b544aba2db3c48e28b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacf27dfed966a8981710c8866a90d5e",
+    "url": "/Indicator/index.html"
+  },
+  {
+    "revision": "587e729ac5c18ab3f733",
+    "url": "/Indicator/static/css/main.db847c10.chunk.css"
+  },
+  {
+    "revision": "818676904d5cd6a62684",
+    "url": "/Indicator/static/js/2.7f76354f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Indicator/static/js/2.7f76354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587e729ac5c18ab3f733",
+    "url": "/Indicator/static/js/main.3a77d6e1.chunk.js"
+  },
+  {
+    "revision": "2a3b89574fa5239358bb",
+    "url": "/Indicator/static/js/runtime-main.91de5ae2.js"
+  }
+]);
